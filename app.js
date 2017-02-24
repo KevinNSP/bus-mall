ff23bba595a46b508a1fb6b64cc88ea614caa68f
Kevin Schmidt <NSPtuning@gmail.com>
2017-02-23 18:39:26 -0800

    chartzzzz

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var totalClicks = 0;
 
 var clickLimit = 0;
 
-var maxClicks = 25;
+var maxClicks = 3;
 
 function CatalogEntry(name, imageSrc){
   this.name = name;
@@ -52,8 +52,8 @@ var chooserImages = function() {
       // console.log(i);
     } else {
       allProductsAr[picks].timesShown += 1;
-      console.log('**-----------------------**');
-      console.log('times shown: ', allProductsAr[picks].timesShown);
+      // console.log('**-----------------------**');
+      // console.log('times shown: ', allProductsAr[picks].timesShown);
       displayedProducts.push(allProductsAr[picks]);
       // console.log(i);
     }
@@ -146,10 +146,68 @@ function handleClick(event) {
   if(totalClicks >= maxClicks) {
     displayList();
     clickCounter.removeEventListener('click', handleClick);
+    myChart();
   }
 
 };
 
+function saveProductsToLocalStorage(allProducts) {
+  localStorage.allProductsAr = JSON.stringify(allProducts);
+};
+saveProductsToLocalStorage(allProductsAr);
+
+// *****=============CHART START=====================**
+
+var chartImageNames = [];
+var chartImageClicks = [];
+var chartImageViews = [];
+
+function chartData() {
+  for (var i = 0; i < allProductsAr.length; i++) {
+    chartImageNames.push(allProductsAr[i].name);
+    chartImageClicks.push(allProductsAr[i].timesClicked);
+    chartImageViews.push(allProductsAr[i].timesShown);
+  }
+
+  console.log(chartImageNames);
+  console.log(chartImageViews);
+  console.log(chartImageClicks);
+}
+
+function myChart() {
+  var ctx = document.getElementById('chart').getContext('2d');
+  chartData();
+  //var data = allProducts[i].clicks;
+  var labelColors = 'blue';
+
+  var myChartData = {
+    type: 'bar',
+    data: {
+      labels: chartImageNames,
+      datasets: [{
+        label: 'Number of Views',
+        data: chartImageViews,
+        backgroundColor: labelColors,
+      },
+      {
+        label: 'Number of Votes',
+        data: chartImageClicks,
+        backgroundColor: '#f58a07',
+      }],
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  };
+
+  var myChart = new Chart(ctx, myChartData);
+}
 // function handleSeen(event) {
 //   event.preventDefault();
 //   event.stopPropagation();
